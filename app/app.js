3d2482471fd37ce1430990012b4ac04c27e3e3a8
aki85 <akiaki8528@gmail.com>
2019-12-06 20:37:11 +0900

    cros

--- app/app.js
+++ app/app.js
@@ -14,6 +14,12 @@ mongoose.connection.on('error', function(err) {
     process.exit(-1)
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
