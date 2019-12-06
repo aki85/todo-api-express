6bbe349f3d02a4365007a1b1ac8a183959676b04
三輪一晶 <>
2019-12-06 22:44:48 +0900

    allow jwt

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ mongoose.connection.on('error', function(err) {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, jwt")
     next()
 })
 
