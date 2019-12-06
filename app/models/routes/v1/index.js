4549a913dd45939027947496f1fee510f7d9af63
三輪一晶 <>
2019-12-06 22:57:40 +0900

    test

--- app/models/routes/v1/index.js
+++ app/models/routes/v1/index.js
@@ -4,7 +4,7 @@ const auth = require('./auth.js')
 
 router.use('/user', require('./user.js'))
 router.use('/auth', auth.router)
-router.use(auth.jwtFilter)
+// router.use(auth.jwtFilter)
 router.use('/todo', require('./todo.js'))
 
 module.exports = router
