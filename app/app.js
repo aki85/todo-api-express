de590143a65f538fef9aa7c62ad4dff35832d999
miwa-win <k.miwa@btree.tech>
2019-12-11 02:30:33 +0900

    api test

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ mongoose.connection.on('error', function(err) {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, jwt")
+    res.header("Access-Control-Allow-Headers", "")
     next()
 })
 
